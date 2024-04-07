332673f3a1c1819757992df535d6d3e5890d3994
Ittalian <bothmather@au.com>
2024-04-08 01:25:26 +0900

    UI途中まで

--- lib/main.dart
+++ lib/main.dart
@@ -10,116 +10,145 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _selectIndex = 0;
 
-  void _incrementCounter() {
+  final _pages = <Widget>[
+    Column(children: [
+      Expanded(
+        child: Image.asset('images/title.jpg'),
+      ),
+      Center(
+        child: Image.asset('images/home.jpg'),
+      ),
+    ]),
+    Column(children: [
+      Container(
+        alignment: Alignment.topCenter,
+        margin: const EdgeInsets.only(top: 50),
+        child: Image.asset('images/registration.jpg'),
+      ),
+      Container(
+        margin: const EdgeInsets.fromLTRB(50, 80, 60, 0),
+        child: const TextField(
+          maxLines: 1,
+          decoration: InputDecoration(
+            icon: Icon(Icons.currency_yen),
+            hintText: '10000',
+            labelText: '金額',
+          ),
+        ),
+      ),
+      const Padding(padding: EdgeInsets.only(top: 50)),
+      const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Text(
+          '項目',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        SizedBox(width: 50),
+        DropdownButtonMenu(),
+      ])
+    ]),
+    Column(children: [
+      Container(
+        alignment: Alignment.topCenter,
+        margin: const EdgeInsets.only(top: 50),
+        child: Image.asset('images/calendar.jpg'),
+      ),
+    ]),
+  ];
+
+  void _onTapItem(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: _pages[_selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+          BottomNavigationBarItem(icon: Icon(Icons.add_chart), label: '収支登録'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: 'カレンダー'),
+        ],
+        currentIndex: _selectIndex,
+        onTap: _onTapItem,
+        type: BottomNavigationBarType.fixed,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class DropdownButtonMenu extends StatefulWidget {
+  const DropdownButtonMenu({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownButtonMenu> createState() => _DropdownButtonMenuState();
+}
+
+class _DropdownButtonMenuState extends State<DropdownButtonMenu> {
+  String isSelectedValue = '食費';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      items: const[
+        DropdownMenuItem(
+          value: '食費',
+          child: Text(
+            '食費',
+            style: TextStyle(
+              fontSize: 20,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        DropdownMenuItem(
+          value: '遊び',
+          child: Text(
+            '遊び',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        DropdownMenuItem(
+          value: '生活費',
+          child: Text(
+            '生活費',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
+      value: isSelectedValue,
+      onChanged: (String? value) {
+        setState(() {
+          isSelectedValue = value!;
+        });
+      },
     );
   }
 }
