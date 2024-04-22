44a205291deb7d54ccd17fb7218b9417f14a7d37
Ittalian <bothmather@au.com>
2024-04-22 20:37:47 +0900

    ListViewのUI実装

--- lib/budget_confirmation.dart
+++ lib/budget_confirmation.dart
@@ -1,52 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'pie_chart_draw.dart';
 import 'bar_chart_draw.dart';
 import 'package:share_kakeibo/main.dart';
 
 class BudgetConfirmation extends StatefulWidget {
   // final String category;
-  final num foodPrice;
-  final num playPrice;
-  final num lifePrice;
   final num monthFoodPrice;
   final num monthPlayPrice;
   final num monthLifePrice;
   final num lastMonthFoodPrice;
   final num lastMonthPlayPrice;
   final num lastMonthLifePrice;
-  final num ichiFoodPrice;
-  final num ichiPlayPrice;
-  final num ichiLifePrice;
   final num ichiMonthFoodPrice;
   final num ichiMonthPlayPrice;
   final num ichiMonthLifePrice;
-  final num moeFoodPrice;
-  final num moePlayPrice;
-  final num moeLifePrice;
   final num moeMonthFoodPrice;
   final num moeMonthPlayPrice;
   final num moeMonthLifePrice;
   final int month;
   final int day;
   const BudgetConfirmation(
-      {required this.foodPrice,
-      required this.playPrice,
-      required this.lifePrice,
-      required this.monthFoodPrice,
+      {required this.monthFoodPrice,
       required this.monthPlayPrice,
       required this.monthLifePrice,
       required this.lastMonthFoodPrice,
       required this.lastMonthPlayPrice,
       required this.lastMonthLifePrice,
-      required this.ichiFoodPrice,
-      required this.ichiPlayPrice,
-      required this.ichiLifePrice,
       required this.ichiMonthFoodPrice,
       required this.ichiMonthPlayPrice,
       required this.ichiMonthLifePrice,
-      required this.moeFoodPrice,
-      required this.moePlayPrice,
-      required this.moeLifePrice,
       required this.moeMonthFoodPrice,
       required this.moeMonthPlayPrice,
       required this.moeMonthLifePrice,
@@ -58,24 +41,15 @@ class BudgetConfirmation extends StatefulWidget {
 }
 
 class _BudgetConfirmationState extends State<BudgetConfirmation> {
-  late num foodPriceState;
-  late num playPriceState;
-  late num lifePriceState;
   late num monthFoodPriceState;
   late num monthPlayPriceState;
   late num monthLifePriceState;
   late num lastMonthFoodPriceState;
   late num lastMonthPlayPriceState;
   late num lastMonthLifePriceState;
-  late num ichiFoodPriceState;
-  late num ichiPlayPriceState;
-  late num ichiLifePriceState;
   late num ichiMonthFoodPriceState;
   late num ichiMonthPlayPriceState;
   late num ichiMonthLifePriceState;
-  late num moeFoodPriceState;
-  late num moePlayPriceState;
-  late num moeLifePriceState;
   late num moeMonthFoodPriceState;
   late num moeMonthPlayPriceState;
   late num moeMonthLifePriceState;
@@ -86,24 +60,15 @@ class _BudgetConfirmationState extends State<BudgetConfirmation> {
   void initState() {
     super.initState();
 
-    foodPriceState = widget.foodPrice;
-    playPriceState = widget.playPrice;
-    lifePriceState = widget.lifePrice;
     monthFoodPriceState = widget.monthFoodPrice;
     monthPlayPriceState = widget.monthPlayPrice;
     monthLifePriceState = widget.monthLifePrice;
     lastMonthFoodPriceState = widget.lastMonthFoodPrice;
     lastMonthPlayPriceState = widget.lastMonthPlayPrice;
     lastMonthLifePriceState = widget.lastMonthLifePrice;
-    ichiFoodPriceState = widget.ichiFoodPrice;
-    ichiPlayPriceState = widget.ichiPlayPrice;
-    ichiLifePriceState = widget.ichiLifePrice;
     ichiMonthFoodPriceState = widget.ichiMonthFoodPrice;
     ichiMonthPlayPriceState = widget.ichiMonthPlayPrice;
     ichiMonthLifePriceState = widget.ichiMonthLifePrice;
-    moeFoodPriceState = widget.moeFoodPrice;
-    moePlayPriceState = widget.moePlayPrice;
-    moeLifePriceState = widget.moeLifePrice;
     moeMonthFoodPriceState = widget.moeMonthFoodPrice;
     moeMonthPlayPriceState = widget.moeMonthPlayPrice;
     moeMonthLifePriceState = widget.moeMonthLifePrice;
@@ -114,6 +79,7 @@ class _BudgetConfirmationState extends State<BudgetConfirmation> {
   @override
   Widget build(BuildContext context) {
     // 円グラフ作成
+    final screenWidth = MediaQuery.of(context).size.width;
     double radius = 50;
     double personalRadius = 40;
     num totalPrice =
@@ -164,7 +130,7 @@ class _BudgetConfirmationState extends State<BudgetConfirmation> {
                   ]))),
           const Padding(padding: EdgeInsets.only(top: 30)),
           Text(
-            "${monthState.toString()}月${dayState.toString()}日の収支",
+            "${monthState.toString()}月の収支",
             style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
@@ -173,59 +139,8 @@ class _BudgetConfirmationState extends State<BudgetConfirmation> {
           const Padding(padding: EdgeInsets.only(top: 30)),
           Row(
             children: [
-              Container(
-                  width: 200,
-                  child: Column(children: [
-                    const Text(
-                      "いちくん",
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                    PieChartDraw(
-                        foodPrice: ichiFoodPriceState,
-                        lifePrice: ichiLifePriceState,
-                        playPrice: ichiPlayPriceState,
-                        radius: personalRadius)
-                  ])),
-              Container(
-                  width: 200,
-                  child: Column(children: [
-                    const Text(
-                      "もえちゃん",
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                    PieChartDraw(
-                        foodPrice: moeFoodPriceState,
-                        lifePrice: moeLifePriceState,
-                        playPrice: moePlayPriceState,
-                        radius: personalRadius)
-                  ])),
-            ],
-          ),
-          const Text(
-            "ふたり",
-            style: TextStyle(fontSize: 15),
-          ),
-          PieChartDraw(
-              foodPrice: foodPriceState,
-              lifePrice: lifePriceState,
-              playPrice: playPriceState,
-              radius: radius),
-          Text(
-            "${monthState.toString()}月全体の収支",
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Padding(padding: EdgeInsets.only(top: 30)),
-          Row(
-            children: [
-              Container(
-                  width: 200,
+              SizedBox(
+                  width: screenWidth * 0.5,
                   child: Column(children: [
                     const Text(
                       "いちくん",
@@ -239,8 +154,8 @@ class _BudgetConfirmationState extends State<BudgetConfirmation> {
                         playPrice: ichiMonthPlayPriceState,
                         radius: personalRadius)
                   ])),
-              Container(
-                  width: 200,
+              SizedBox(
+                  width: screenWidth * 0.5,
                   child: Column(children: [
                     const Text(
                       "もえちゃん",
