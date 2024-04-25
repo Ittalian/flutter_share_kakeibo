3f38721ffd75398bda59e33d995f0171efc904fa
Ittalian <bothmather@au.com>
2024-04-26 00:55:53 +0900

    不要なimport削除

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'dropdown_button_menu.dart';
 import 'user_dropdown_button_menu.dart';
 
