0e3fb3179c4e55f53f0ccc1e03924c0c6ab04024
Ittalian <bothmather@au.com>
2024-04-27 02:41:09 +0900

    キャッシュ削除

--- android/app/src/main/kotlin/com/example/share_kakeibo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.share_kakeibo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/price_list.dart
+++
@@ -1,7 +0,0 @@
-class PriceList {
-  num foodPrice;
-  num playPrice;
-  num lifePrice;
-
-  PriceList(this.foodPrice, this.playPrice, this.lifePrice);
-}
